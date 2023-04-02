0a221873d205ea849dfc62fc12499fcbf5a1b273
d-lynch95 <darraghlynch1995@gmail.com>
2023-04-02 23:35:15 +0000

    add creds file

--- run.py
+++ run.py
@@ -62,8 +62,15 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated succesfully \n")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+
+
+main()
 
 
